f53313e90ae219381125771cc3a4b98281bcd9f4
Muhammad Dhafin <work.dhafin@gmail.com>
2020-09-22 13:35:06 +0700

    strings example deeper

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,5 +4,6 @@ let currentResult = defaultResult;
 currentResult = ((currentResult + 10) * 3) / 2 - 1;
 
 let calculationDescription = `( ${defaultResult} + 10) * 3 / 2 - 1`;
+let errorMassage = 'An error \n' + 'occureed!';
 
-outputResult(currentResult, calculationDescription);
+outputResult(currentResult, errorMassage);
